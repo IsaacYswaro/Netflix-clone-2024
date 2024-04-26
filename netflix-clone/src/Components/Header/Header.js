fb3ee5946455cb357e6c637bd876aa3144cdab2e
IsaacYswaro <yisakeswaro@gmail.com>
2024-04-25 21:58:07 -0500

    Header and footer

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import CastConnectedIcon from "@mui/icons-material/CastConnected";
+
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul className="nav_menu">
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>Movies</li>
+            <li>TVShows</li>
+            <li>Top</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Language</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul className="header_icons">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <CastConnectedIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
