2d97ad660bc7f7a61d835be8b289ce5950061848
IsaacYswaro <yisakeswaro@gmail.com>
2024-05-03 22:35:57 -0500

    Edited Responsiveness

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -9,39 +9,135 @@ const Footer = () => {
     <>
       <div className="footer_outer_container">
         <div className="footer_inner_container">
-          <div className="                                                                                                                                                                                                                         ">
+          <div className="footer_icons">
             <FacebookIcon />
             <InstagramIcon />
-            <TwitterIcon/>
+            <TwitterIcon />
             <YouTubeIcon />
           </div>
           <div className="footer_data">
             <div>
               <ul>
-                <li>Audio Descrpition</li>
-                <li> Investor Relations</li>
-                <li>Legal Notice</li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://www.netflix.com/browse/audio-description"
+                  >
+                    <span className="member-footer-link-content">
+                      Audio Description
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://ir.netflix.net/ir-overview/profile/default.aspx"
+                  >
+                    <span className="member-footer-link-content">
+                      Investor Relations
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://help.netflix.com/legal/privacy"
+                  >
+                    <span className="member-footer-link-content">Privacy</span>
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <ul>
-                <li>Help Center</li>
-                <li> Jobs</li>
-                <li>Cookie Preference</li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://help.netflix.com/en/"
+                  >
+                    <span className="member-footer-link-content">
+                      Help Center
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a className="footer_link" href="https://jobs.netflix.com/">
+                    <span className="member-footer-link-content">Jobs</span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://help.netflix.com/legal/notices"
+                  >
+                    <span className="member-footer-link-content">
+                      Legal Notice
+                    </span>
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <ul>
-                <li>Gift Cards</li>
-                <li> Terms of Use</li>
-                <li>Corporate Information</li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://www.netflix.com/redeem"
+                  >
+                    <span className="member-footer-link-content">
+                      Gift Cards
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a className="footer_link" href="https://www.netflix.shop/">
+                    <span className="member-footer-link-content">
+                      Netflix Shop
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://www.netflix.com/browse"
+                  >
+                    <span className="member-footer-link-content">
+                      Cookie Preferences
+                    </span>
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <ul>
-                <li>Media Center</li>
-                <li>Privacy</li>
-                <li>Contact Us</li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://media.netflix.com/en/"
+                  >
+                    <span className="member-footer-link-content">
+                      MediaCenter
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://help.netflix.com/legal/termsofuse"
+                  >
+                    <span className="member-footer-link-content">
+                      Terms of Use
+                    </span>
+                  </a>
+                </li>
+                <li className="footer_link_wrapper">
+                  <a
+                    className="footer_link"
+                    href="https://help.netflix.com/en/node/134094"
+                  >
+                    <span className="member-footer-link-content">Corporate Information</span>
+                  </a>
+                </li>
               </ul>
             </div>
             <div className="service_code">
